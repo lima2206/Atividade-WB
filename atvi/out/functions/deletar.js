6ce2876c7e4a2c578a9452a80cce17fee68f47c9
Vitor Spricigo Lima <112436453+lima2206@users.noreply.github.com>
2023-12-03 17:24:05 -0300

    Delete atvi directory

--- atvi/out/functions/deletar.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Deletar = /** @class */ (function () {
-    function Deletar() {
-    }
-    return Deletar;
-}());
-exports.default = Deletar;

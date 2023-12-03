bc6d18cbe4525e9c45020e29baf2ef959b5ec5e6
Vitor Spricigo Lima <112436453+lima2206@users.noreply.github.com>
2023-12-03 17:26:57 -0300

    Delete atvi directory

--- atvi/out/functions/abstract/atualizacao.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var atualizacao = /** @class */ (function () {
-    function atualizacao() {
-    }
-    return atualizacao;
-}());
-exports.default = atualizacao;
